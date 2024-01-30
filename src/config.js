569b162bc1ddc9619fd2a057b61cb78ecb0f95cd
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2024-01-30 00:28:05 -0300

    cambios en config db

--- src/config.js
+++ src/config.js
@@ -1,5 +1,16 @@
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT;
+
+const DB_USER = process.env.DB_USER;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
+const DB_HOST = process.env.DB_HOST;
 
 module.exports = {
   PORT,
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
 };
