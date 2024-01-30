2d0e6657be9a7b3595cb0004491ec1737a220caa
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2024-01-29 21:17:53 -0300

    agrega configuracion para el deploy en railway

--- cleanWave/src/app.js
+++ cleanWave/src/app.js
@@ -1,50 +1,49 @@
 // ************** Require's ******************
-const express = require("express");
-const path = require("path");
-const methodOverride = require("method-override");
-const session = require("express-session");
-const userLoggedMiddleware = require("./middlewares/userLoggedMiddleware");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const notFound = require("./middlewares/404Middleware")
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const notFound = require('./middlewares/404Middleware');
 
 // ********** express() ******************
 const app = express();
 
 // ********* Middlewares *****************
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(
   express.urlencoded({ extended: false })
 ); /* esto nos permite capturar en req.body la info de un formulario que se envia via post */
 app.use(express.json());
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 app.use(
   session({
-    secret: "cleanWave",
+    secret: 'cleanWave',
     resave: false,
     saveUninitialized: false,
   })
 );
 app.use(cookieParser());
 app.use(userLoggedMiddleware);
-app.use(cors())
+app.use(cors());
 
-const destroySearchProductSession = require("./middlewares/destroySearchProductsSession");
+const destroySearchProductSession = require('./middlewares/destroySearchProductsSession');
 app.use(destroySearchProductSession);
 
-
 // **************** Template Engine *********************
-app.set("view engine", "ejs");
-app.set("views", "./src/views");
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
 
 // ********** Route system require and use() **************
-const mainRouter = require("./routes/mainRouter");  
+const mainRouter = require('./routes/mainRouter');
 app.use(mainRouter);
-app.get("/", (req, res) => {
-  res.redirect("/consumidorfinal");
+app.get('/', (req, res) => {
+  res.redirect('/consumidorfinal');
 });
-app.use(notFound)
+app.use(notFound);
 
 // *************** Iniciar el servidor web *****************
-const PORT = 3333;
+const { PORT } = require('./config.js');
 app.listen(PORT, () => console.log(`Servidor creado en el puerto ${PORT}`));
--- cleanWave/src/database/config/config.js
+++ cleanWave/src/database/config/config.js
@@ -1,23 +1,34 @@
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+} = require('../../config.js');
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "cleanwave",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: 'mysql',
+    port: DB_PORT,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: DB_PORT,
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: DB_PORT,
+  },
+};
