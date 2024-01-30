1d0d9b44a8a0a8854bfe2e2660d44616c6efdef6
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2024-01-29 23:11:00 -0300

    corrije error database config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,16 +8,16 @@ module.exports = {
   },
   test: {
     username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    password: 'e2HHGdBc4DFcFC6eA5A4GHda-gec1CB4',
+    database: 'railway',
+    host: 'viaduct.proxy.rlwy.net',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    password: 'e2HHGdBc4DFcFC6eA5A4GHda-gec1CB4',
+    database: 'railway',
+    host: 'viaduct.proxy.rlwy.net',
     dialect: 'mysql',
   },
 };
