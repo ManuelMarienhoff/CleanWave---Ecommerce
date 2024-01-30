e390c45314a7b539bc663ff95aae47d2459eed9b
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2024-01-29 23:17:18 -0300

    cambia mayus por minus en modelos products

--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -1,51 +1,48 @@
-module.exports = (sequelize, dataTypes)=>{
-    const alias = "Product";
-    const cols = {
-      id: {
-        type: dataTypes.STRING(255),
-        primaryKey: true,
-      },
-      name: dataTypes.STRING(255),
-      shortName: dataTypes.STRING(255),
-      retailPrice: dataTypes.DECIMAL(10, 2),
-      wholesalePrice: dataTypes.DECIMAL(10, 2),
-      discountCf: dataTypes.INTEGER,
-      discountM: dataTypes.INTEGER,
-      stock: dataTypes.INTEGER,
-      image: dataTypes.STRING(255),
-      category_id: dataTypes.INTEGER,
-      description: dataTypes.TEXT,
-      brand_id: dataTypes.INTEGER,
-      sold: dataTypes.INTEGER,
-      offer: dataTypes.BOOLEAN
-    };
-    const config= {
-        tableName: "Products",
-        timestamps: false
-    }
+module.exports = (sequelize, dataTypes) => {
+  const alias = 'Product';
+  const cols = {
+    id: {
+      type: dataTypes.STRING(255),
+      primaryKey: true,
+    },
+    name: dataTypes.STRING(255),
+    shortName: dataTypes.STRING(255),
+    retailPrice: dataTypes.DECIMAL(10, 2),
+    wholesalePrice: dataTypes.DECIMAL(10, 2),
+    discountCf: dataTypes.INTEGER,
+    discountM: dataTypes.INTEGER,
+    stock: dataTypes.INTEGER,
+    image: dataTypes.STRING(255),
+    category_id: dataTypes.INTEGER,
+    description: dataTypes.TEXT,
+    brand_id: dataTypes.INTEGER,
+    sold: dataTypes.INTEGER,
+    offer: dataTypes.BOOLEAN,
+  };
+  const config = {
+    tableName: 'products',
+    timestamps: false,
+  };
 
-    const Product = sequelize.define(alias,cols,config);
+  const Product = sequelize.define(alias, cols, config);
 
-    Product.associate = function(models){
-        Product.belongsTo(models.Category, {
-            as: "category",
-            foreignKey: "category_id"
-        });
+  Product.associate = function (models) {
+    Product.belongsTo(models.Category, {
+      as: 'category',
+      foreignKey: 'category_id',
+    });
 
-        Product.belongsTo(models.Brand, {
-            as: "brand",
-            foreignKey: "brand_id"
-        });
-        Product.belongsToMany(models.Cart, {
-            as: "cart",
-            through: "CartProduct",
-            foreignKey: "product_id",
-            otherKey: "cart_id"
-        });
-    }
-    
+    Product.belongsTo(models.Brand, {
+      as: 'brand',
+      foreignKey: 'brand_id',
+    });
+    Product.belongsToMany(models.Cart, {
+      as: 'cart',
+      through: 'CartProduct',
+      foreignKey: 'product_id',
+      otherKey: 'cart_id',
+    });
+  };
 
-    return Product
-
-
-}
+  return Product;
+};
